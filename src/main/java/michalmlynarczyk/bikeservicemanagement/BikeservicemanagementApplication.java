eee1968ec79a49688d1bae50a8a6a5ee556a7b97
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-08 12:50:13 +0100

    initial

---
+++ src/main/java/michalmlynarczyk/bikeservicemanagement/BikeservicemanagementApplication.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.bikeservicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeservicemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeservicemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/michalmlynarczyk/bikeservicemanagement/BikeservicemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.bikeservicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeservicemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
