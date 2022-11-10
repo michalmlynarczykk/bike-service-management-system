770ab0811bcb6bd466b2b1d7012b3384e0e2c903
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-10 11:20:33 +0100

    add: getters/setters, name refactor

--- src/main/java/michalmlynarczyk/bikeservicemanagement/part/Part.java
+++ src/main/java/michalmlynarczyk/bikeservicemanagement/part/Part.java
@@ -1,9 +1,14 @@
 package michalmlynarczyk.bikeservicemanagement.part;
 
-
 import michalmlynarczyk.bikeservicemanagement.order.Order;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -11,11 +16,11 @@ import javax.validation.constraints.NotNull;
 public class Part {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
+    private Long partId;
 
     @Column(length = 100)
     @NotBlank
-    private String name;
+    private String partName;
 
     @NotNull
     private Double price;
@@ -25,16 +30,52 @@ public class Part {
 
     @ManyToOne
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Order repairOrder;
 
     public Part() {
 
     }
 
-    public Part(String name, Double price, int quantity, Order order) {
-        this.name = name;
+    public Part(String partName, Double price, int quantity, Order repairOrder) {
+        this.partName = partName;
         this.price = price;
         this.quantity = quantity;
-        this.order = order;
+        this.repairOrder = repairOrder;
+    }
+
+    public Long getPartId() {
+        return partId;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Order getRepairOrder() {
+        return repairOrder;
+    }
+
+    public void setRepairOrder(Order repairOrder) {
+        this.repairOrder = repairOrder;
     }
 }
